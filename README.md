2a0fb5f07c9141ca07eab32524eabbadde00186c
Nishank <nishankpr2002@gmail.com>
2022-01-29 18:22:04 +0530

    refractored something

--- README.md
+++
@@ -1,8 +0,0 @@
-- This project is build by appplying the three-schema database-architecture.
-
-## Tech used in this project:
-
-- Java
-- Spring
-- Apache Tomcat
-- Docker
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.amigoscode.demo;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
 }
