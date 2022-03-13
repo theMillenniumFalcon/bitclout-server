94e5038af70b1e1a2ecafe43ff79282e78723416
Nishank <nishankpr2002@gmail.com>
2022-03-14 00:18:05 +0530

    updated

--- dist/constants/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FORGOT_PASSWORD_PREFIX = exports.COOKIE = exports.__prod__ = void 0;
-exports.__prod__ = process.env.NODE_ENV === "production";
-exports.COOKIE = 'cookie';
-exports.FORGOT_PASSWORD_PREFIX = 'forgot-password:';
-//# sourceMappingURL=constants.js.map
